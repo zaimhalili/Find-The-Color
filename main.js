ade4276217fb3f7cf1420e184e308992c5387a28
Zaim Halili <halilizaim@gmail.com>
2025-07-24 23:54:13 +0200

    created the rgb to hex converter

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-const componentToHex = (c) => {
-    const hex = c.toString(16);
+const componentToHex = (e) => {
+    const hex = e.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 }
 
 const rgbToHex = (r, g, b) => {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}  
+}
 
-generateColors = () => {
-    const elem = document.getElementById('2');
+// generateColors = () => {
+
+// }
+const len = document.getElementsByClassName('color-block').length;
+
+for (let i = 0; i < len - 1; i++) {
+    let elem = document.getElementById(i + 1);
     const bg = window.getComputedStyle(elem).backgroundColor;
-    alert(rgbToHex(bg));
+    const rgbNumbers = bg.slice(4, -1);
+    const [r, g, b] = rgbNumbers.split(',').map(val => parseInt(val.trim()));
+    alert(rgbToHex(r, g, b));
+    elem.innerText = rgbToHex(r, g, b).toString();
+    window.getComputedStyle(elem).fontSize = 100;
 }
