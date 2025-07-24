c67ee78dc0d62294b38f94c4346ee2529d8b277d
Zaim Halili <halilizaim@gmail.com>
2025-07-24 13:57:25 +0200

    Added the rgb extractor to show the color of the blocks

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+const componentToHex = (c) => {
+    const hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
 
+const rgbToHex = (r, g, b) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}  
 
 generateColors = () => {
-    const colors = document.getElementsByClassName('.color-block');
+    const elem = document.getElementById('2');
+    const bg = window.getComputedStyle(elem).backgroundColor;
+    alert(rgbToHex(bg));
 }
